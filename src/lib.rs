42e91a0c2620b9cc2b393c055d5c28f6b43d6479
hmb <humberto.munozb@gmail.com>
2019-12-18 18:54:46 -0800

    Groundwork for bosonic operators

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,21 @@ extern crate alga;
 extern crate nalgebra;
 extern crate log;
 
+use alga::general::{RealField, ComplexField};
+use blas_traits::BlasScalar;
+use num_complex::Complex32 as c32;
+use num_complex::Complex64 as c64;
+
+pub trait ComplexScalar<R: RealField> :
+    ComplexField<RealField=R> + BlasScalar {}
+impl ComplexScalar<f32> for f32{}
+impl ComplexScalar<f64> for f64{}
+impl ComplexScalar<f32> for num_complex::Complex<f32>{}
+impl ComplexScalar<f64> for num_complex::Complex<f64>{}
 
 pub mod util;
-//pub mod algebra;
+pub mod algebra;
 pub mod base;
 pub mod oqs;
 pub mod ode;
 
-//mod tests {
-//    #[test]
-//    fn it_works() {
-//        assert_eq!(2 + 2, 4);
-//    }
-//}
